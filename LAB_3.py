2018372d4c3cc82cedcd264e64c178ff76b13df6
Soham Chatterjee <76518496+Sohamrio@users.noreply.github.com>
2023-08-08 16:33:13 +0530

    Add files via upload

--- LAB_3.py
+++ LAB_3.py
@@ -173,7 +173,22 @@ else:
         print("Pay :",pay)
         
 """
-        
+#9
+
+num = int(input("Enter a number: "))
+sum = 0
+temp = num
+while temp > 0:
+   digit = temp % 10
+   sum += digit ** 3
+   temp //= 10
+
+
+if num == sum:
+   print(num,"is an Armstrong number")
+else:
+   print(num,"is not an Armstrong number")
+
 
 
 
