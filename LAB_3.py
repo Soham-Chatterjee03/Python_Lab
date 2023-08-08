d46184d644652fd2437fe887f898391b9113fe04
Soham Chatterjee <76518496+Sohamrio@users.noreply.github.com>
2023-08-08 16:30:29 +0530

    Add files via upload

--- LAB_3.py
+++ LAB_3.py
@@ -92,7 +92,7 @@ else:
 print("Pay :",pay)
 """
 #3
-
+"""
 score=float(input("Enter the score between the reange 0.0-1.0 :"))
 if((score>0.0) and (score<1.0)):
         if(score>=0.9):
@@ -107,9 +107,72 @@ if((score>0.0) and (score<1.0)):
             print("F")
 else:
     raise Exception("Enter the value in the given range")    
+"""
 
-    
+#4
+"""
+choice=int(input(""1.For Farhenite to Celcious
+2.For Celcious to farhenite""))
+if(choice==1):
+        F=float(input("Enter the temperature in farhenite:"))
+        C=(F-32)*(5/9)
+        print("THe temp in Celcious is",C)
+elif(choice==2):
+        C=float(input("Enter the temperature in celcious:"))
+        F=(C*(9/5))+32
+        print("THe temp in Farhenite is",F)
+else:
+        print("Wrong input")
+"""
 
+#5
+"""
+num=int(input("Enter a number"))
+if(num>=0):
+        print("+ve")
+else:
+        print("-ve")
+"""
+#6
+"""
+num=int(input("Enter a number"))
+if(num%2==0):
+        print("even")
+else:
+        print("Odd")
+"""
+#7
+"""
+
+a=int(input("Enter A "))
+b=int(input("Enter B "))
+c=int(input("Enter C "))
+if(a>b and a>c):
+        print("A is largest")
+elif(b>a and b>c):
+        print("B is largest")
+else:
+        print("C is largest")
+
+"""
+#8
+"""
+try:
+        rate=int(input("Rate"))
+        hour=int(input("Hours"))
+except:
+        print("Error,Please enter numeric form")
+        x="a"
+if x is "a":
+        pass
+elif(hour<=40):
+        pay=hour*rate
+        print("Pay :",pay)
+else:
+        pay=40*rate+1.5*rate*(hour-40)
+        print("Pay :",pay)
+        
+"""
         
 
 
